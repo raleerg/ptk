0f197a150a58c3153db8f3860e5a1032c8de018a
Marko Raskovic <markoraskovic@Markos-MBP.fritz.box>
2017-07-09 15:40:22 +0200

    Constants added

---
+++ src/lib/Constants.php
@@ -0,0 +1,19 @@
+<?php
+namespace App\Lib;
+
+class Constants
+{
+    const URL_BASE = 'http://webshop.politika.rs/';
+    const URL_LOGIN = 'site/login';
+    const URL_READ = 'read';
+    const URL_HOME = 'magazine/politika-1.html';
+    const URL_ISSUE_PAGES_IMAGES = 'image/page/politika-{IN}/article/politika-beograd/page/{PGN}.jpeg';
+
+    const USER_NAME = '';
+    const PASSWORD = '';
+
+    const USER_NAME_FIELD = 'LoginForm[username]';
+    const PASSWORD_FIELD = 'LoginForm[password]';
+
+    const DOWNLOAD_FOLDER = 'downloads/';
+}
